68b877aa6237f28d5d3103d1ac5f48bcd72179d1
Eduardo S. Libardi <eslibardi@gmail.com>
2025-10-22 18:12:33 -0300

    shadcn and additional boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,17 @@
+import {Button} from '@/components/ui/button'
 const App = () => {
-  return <h1 className='text-2xl font-bold'>Search Github Users</h1>
+  return (
+    <div className='flex  items-center justify-center h-screen'>
+      <div className='flex gap-4'>
+        <Button>Click me</Button>
+        <Button variant='outline' size='lg'>
+          Click me
+        </Button>
+        <Button variant='destructive' size='sm'>
+          Click me
+        </Button>
+      </div>
+    </div>
+  )
 }
 export default App
