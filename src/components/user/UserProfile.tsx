3faaf5f7c9c1d50731850a30a82ca575e08b00c5
Eduardo S. Libardi <eslibardi@gmail.com>
2025-10-24 12:17:03 -0300

    Add charts to UserProfile

--- src/components/user/UserProfile.tsx
+++ src/components/user/UserProfile.tsx
@@ -3,6 +3,10 @@ import {type UserData} from '@/types'
 import {useQuery} from '@apollo/client/react'
 import UserCard from './UserCard'
 import StatsContainer from './StatsContainer'
+import ForkedRepos from '../charts/ForkedRepos'
+import PopularRepos from '../charts/PopularRepos'
+import UsedLanguages from '../charts/UsedLanguages'
+import Loading from './Loading'
 
 type UserProfileProps = {
   userName: string
@@ -13,7 +17,7 @@ const UserProfile = ({userName}: UserProfileProps) => {
     variables: {login: userName},
   })
 
-  if (loading) return <div>Loading...</div>
+  if (loading) return <Loading />
   if (error) return <h2 className='text-xl'>{error.message}</h2>
   if (!data) return <h2 className='text-xl'>User Not Found.</h2>
 
@@ -29,6 +33,13 @@ const UserProfile = ({userName}: UserProfileProps) => {
         following={following.totalCount}
         gists={gists.totalCount}
       />
+      {repositories.totalCount > 0 && (
+        <div className='grid md:grid-cols-2 gap-4'>
+          <UsedLanguages repositories={repositories.nodes} />
+          <PopularRepos repositories={repositories.nodes} />
+          <ForkedRepos repositories={repositories.nodes} />
+        </div>
+      )}
     </div>
   )
 }
